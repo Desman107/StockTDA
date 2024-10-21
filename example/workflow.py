c31fb5a053a7e4f40ca21e424d3777e7079cdb68
Dell_Window <2548538192@qq.com>
2024-10-21 17:06:18 +0800

    feat(workflow) : update workflow

--- example/workflow.py
+++ example/workflow.py
@@ -1,19 +1,47 @@
+from StockTDA.model import TDALSTM, TDALightGBM, TDAXGBoost, TDASVM
+from StockTDA.model.BinaryClassification import BinaryClassificationModel
+from StockTDA.TDA import StockTDAConstituentCloud, StockTDAReturnSeriesCloud
+from StockTDA.TDA.TDAFrame import StockTDAFrame
 
-from StockTDA.model.XGB import TDAXGBoost
-from StockTDA.TDA.TDAReturnSeriesCloud import StockTDAReturnSeriesCloud
 from StockTDA.evluate.evaluator import StockTDAClassificationEvaluator
 from StockTDA.data.data_prepare import prepare_formulaic_factor
-from StockTDA import config
-import os
-import inspect
-import joblib
-prepare_formulaic_factor() # for  the first time, place run this
-ClassificationModel = TDAXGBoost()
-TDAModel = StockTDAReturnSeriesCloud()
-TDAModel.all_Features()
-all_df = TDAModel.all_df
 
 
-Evaluator = StockTDAClassificationEvaluator(TDAModel,ClassificationModel)
+from typing import List, Iterator, Tuple
 
-Evaluator.evaluate_all_combinations()
+def cartesian_product(
+        list1: List[StockTDAFrame], 
+        list2: List[BinaryClassificationModel]
+    ) -> Iterator[Tuple[StockTDAFrame, BinaryClassificationModel]]:
+    for item1 in list1:
+        for item2 in list2:
+            yield (item1, item2)
+
+
+prepare_formulaic_factor() # for the first time, place run this function
+ClassificationModel1 = TDAXGBoost()
+ClassificationModel2 = TDALightGBM()
+ClassificationModel3 = TDALSTM()
+ClassificationModel4 = TDASVM()
+
+TDAModel1 = StockTDAReturnSeriesCloud()
+TDAModel1.all_Features()
+
+TDAModel2 = StockTDAConstituentCloud()
+TDAModel2.all_Features()
+
+
+for pair in cartesian_product(
+        [
+            TDAModel1,
+            TDAModel2,
+        ], 
+        [
+            ClassificationModel1,
+            ClassificationModel2,
+            ClassificationModel3,
+            ClassificationModel4,
+        ]
+    ):
+    Evaluator = StockTDAClassificationEvaluator(pair[0],pair[1])
+    Evaluator.evaluate_all_combinations()
