b2f90cd9ff348fabdf687667fee237caa2592392
Dell_Window <2548538192@qq.com>
2024-10-21 17:08:47 +0800

    refactor : add __init__.py to model and TDA module

---
+++ StockTDA/TDA/__init__.py
@@ -0,0 +1,12 @@
+# -*-coding:utf-8 -*-
+
+"""
+# File       : __init__.py
+# Time       : 2024/10/21 13:01
+# Author     : DaZhi Huang
+# Email      : 2548538192@qq.com
+# Description: TDA model definition (especially in proint cloud definition)
+"""
+
+from .TDAConstituentCloud import StockTDAConstituentCloud
+from .TDAReturnSeriesCloud import StockTDAReturnSeriesCloud
---
+++ StockTDA/model/__init__.py
@@ -0,0 +1,13 @@
+# -*-coding:utf-8 -*-
+
+"""
+# File       : __init__.py
+# Time       : 2024/10/21 13:01
+# Author     : DaZhi Huang
+# Email      : 2548538192@qq.com
+# Description: classification model definition
+"""
+from .XGB import TDAXGBoost
+from .LGBM import TDALightGBM
+from .LSTM import TDALSTM
+from .SVM import TDASVM
